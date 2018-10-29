9b8acc200c8797b566a840e4854a55e7018c336b
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-10-29 18:45:30 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-Assignments/m1/1.1 Graph/1.1 Graph/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+interface Graph {
+	public int V();
+	public int E();
+	public void addEdge(int v, int w);
+	public Iterable<Integer> adj(int v);
+	public boolean hasEdge(int v, int w);
+}
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.nextLine();
+		if (str1.equals("List")) {
+			// int noOfVertices = Integer.parseInt(sc.nextLine());
+			// Int noOfEdges = Integer.parseInt(sc.nextLine());
+			// String[] arra1 = sc.nextLine().split(",");
+
+
+		}
+		if ((str1.equals("Matrix"))) {
+			int noOfVertices = Integer.parseInt(sc.nextLine());
+			int noOfEdges = Integer.parseInt(sc.nextLine());
+			String[] arra1 = sc.nextLine().split(",");
+			int[][] graph = new int[noOfVertices][noOfVertices];
+			while (noOfEdges > 0) {
+				String str2 = sc.nextLine();
+				String arra2[] = str2.split(" ");
+				graph[Integer.parseInt(arra2[0])][Integer.parseInt(arra2[0])] = 1;
+				noOfEdges--;
+			}
+			for (int[] each : graph) {
+				System.out.println(Arrays.toString(each));
+			}
+		}
+	}
+}
+
