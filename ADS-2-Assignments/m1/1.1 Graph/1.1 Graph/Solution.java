f50d1b4d3da72530865f3e6fec0b7136af865508
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-10-30 17:07:33 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m1/1.1 Graph/1.1 Graph/Solution.java
+++ ADS-2-Assignments/m1/1.1 Graph/1.1 Graph/Solution.java
@@ -10,88 +10,119 @@ import java.util.Collections;
  * Interface for graph.
  */
 interface Graph {
-	public int V();
-	public int E();
-	public void addEdge(int v, int w);
-	public Iterable<Integer> adj(int v);
-	public boolean hasEdge(int v, int w);
+    /**
+     * int method
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int V();
+    /**
+     * int method;
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int E();
+    /**
+     * Adds an edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
+    public void addEdge(int v, int w);
+    /**
+     * { function_description }
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Integer> adj(int v);
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(int v, int w);
 }
 /**
  * Class for solution.
  */
 class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * reads the input and checks the condition.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String type = sc.nextLine();
-		String[] nodes = new String[2];
-		int v, e1;
-		if (type.equals("List")) {
+    }
+    /**
+     * reads the input and checks the condition.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String type = sc.nextLine();
+        String[] nodes = new String[2];
+        int v, e1;
+        if (type.equals("List")) {
 
-			v = Integer.parseInt(sc.nextLine());
-			e1 = Integer.parseInt(sc.nextLine());
-			String[] input = sc.nextLine().split(",");
+            v = Integer.parseInt(sc.nextLine());
+            e1 = Integer.parseInt(sc.nextLine());
+            String[] input = sc.nextLine().split(",");
 
-			GraphList g = new GraphList(v);
-			for (int i = 0; i < e1; i++) {
-				String[] add = sc.nextLine().split(" ");
-				int a = Integer.parseInt(add[0]);
-				int b = Integer.parseInt(add[1]);
-				if (a != b && !g.hasEdge(a, b)) {
-					g.addEdge(a, b);
-				}
-			}
-			System.out.println(g.display(input));
-		} else if (type.equals("Matrix")) {
-			v = 0;
-			e1 = 0;
-			v = Integer.parseInt(sc.nextLine());
-			e1 = Integer.parseInt(sc.nextLine());
-			String[] line = sc.nextLine().split(",");
-			int[][] graph = new int[v][v];
-			int a, b;
-			while (sc.hasNext()) {
-				nodes = sc.nextLine().split(" ");
-				a = Integer.parseInt(nodes[0]);
-				b = Integer.parseInt(nodes[1]);
-				if (a != b) {
-					graph[a][b] = 1;
-					if (graph[b][a] == 1) {
-						e1--;
-					} else {
-						graph[b][a] = 1;
-					}
-				}
-			}
-			if (v == 1) {
-				System.out.println(v + " vertices, " + (e1 - 1) + " edges");
+            GraphList g = new GraphList(v);
+            for (int i = 0; i < e1; i++) {
+                String[] add = sc.nextLine().split(" ");
+                int a = Integer.parseInt(add[0]);
+                int b = Integer.parseInt(add[1]);
+                if (a != b && !g.hasEdge(a, b)) {
+                    g.addEdge(a, b);
+                }
+            }
+            System.out.println(g.display(input));
+        } else if (type.equals("Matrix")) {
+            v = 0;
+            e1 = 0;
+            v = Integer.parseInt(sc.nextLine());
+            e1 = Integer.parseInt(sc.nextLine());
+            String[] line = sc.nextLine().split(",");
+            int[][] graph = new int[v][v];
+            int a, b;
+            while (sc.hasNext()) {
+                nodes = sc.nextLine().split(" ");
+                a = Integer.parseInt(nodes[0]);
+                b = Integer.parseInt(nodes[1]);
+                if (a != b) {
+                    graph[a][b] = 1;
+                    if (graph[b][a] == 1) {
+                        e1--;
+                    } else {
+                        graph[b][a] = 1;
+                    }
+                }
+            }
+            if (v == 1) {
+            System.out.println(v + " vertices, " + (e1 - 1) + " edges");
 
-			} else {
-				System.out.println(v + " vertices, " + e1 + " edges");
+            } else {
+                System.out.println(v + " vertices, " + e1 + " edges");
 
-			}
-			if (v <= 1 ) {
-				System.out.println("No edges");
-			} else {
-				for (int[] each : graph) {
-					for (int each2 : each) {
-						System.out.print(each2 + " ");
-					}
-					System.out.println();
-				}
-			}
+            }
+            if (v <= 1) {
+                System.out.println("No edges");
+            } else {
+                for (int[] each : graph) {
+                    for (int each2 : each) {
+                        System.out.print(each2 + " ");
+                    }
+                    System.out.println();
+                }
+            }
 
 
-		}
-	}
+        }
+    }
 }
