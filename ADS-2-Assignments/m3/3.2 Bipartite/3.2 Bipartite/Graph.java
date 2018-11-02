f40976e9ec299ab51eac8f96d3c56c8f933cc32e
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-11-02 10:08:17 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-Assignments/m3/3.2 Bipartite/3.2 Bipartite/Graph.java
@@ -0,0 +1,124 @@
+/**
+ * Class for graph.
+ */
+public class Graph {
+    /**
+     * string.
+     */
+    private static final String NEWLINE = System.getProperty("line.separator");
+    /**
+     * verices.
+     */
+    private final int vertices;
+    /**
+     * edges.
+     */
+    private int edges;
+    /**
+     * bags list.
+     */
+    private Bag<Integer>[] adj;
+
+    /**
+     * Initializes an empty graph with {@code V} vertices and 0 edges.
+     * param V the number of vertices
+     *
+     * @param  v number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
+    public Graph(final int v) {
+        this.vertices = v;
+        this.edges = 0;
+        adj = (Bag<Integer>[]) new Bag[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new Bag<Integer>();
+        }
+    }
+
+    /**
+     * Returns the number of vertices in this graph.
+     *
+     * @return the number of vertices in this graph
+     */
+    public int vertices() {
+        return vertices;
+    }
+
+    /**
+     * Returns the number of edges in this graph.
+     *
+     * @return the number of edges in this graph
+     */
+    public int edges() {
+        return edges;
+    }
+
+    /**
+     * validating Vertex.
+     *
+     * @param      v     { parameter_description }
+     */
+    private void validateVertex(final int v) {
+        if (v < 0 || v >= vertices) {
+            throw new IllegalArgumentException("vertex "
+                 + v + " is not between 0 and " + (vertices - 1));
+        }
+    }
+
+    /**
+     * Adds the undirected edge v-w to this graph.
+     *
+     * @param  v one vertex in the edge
+     * @param  w the other vertex in the edge
+     * @throws IllegalArgumentException unless
+     *  both {@code 0 <= v < V} and {@code 0 <= w < V}
+     */
+    public void addEdge(final int v, final int w) {
+        validateVertex(v);
+        validateVertex(w);
+        edges++;
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(final int v, final int w) {
+        for (int each : adj[w]) {
+            if (each == v) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns the vertices adjacent to vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the vertices adjacent to vertex {@code v}, as an iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Integer> adj(final int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    /**
+     * Returns the degree of vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the degree of vertex {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int degree(final int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+}
