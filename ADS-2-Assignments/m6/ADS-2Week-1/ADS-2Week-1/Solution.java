34d4e828e85705e34d5481c7f414c82cf20a5fd3
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-11-04 15:25:34 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m6/ADS-2Week-1/ADS-2Week-1/Solution.java
+++ ADS-2-Assignments/m6/ADS-2Week-1/ADS-2Week-1/Solution.java
@@ -4,53 +4,48 @@ import java.util.ArrayList;
 
 
 class PageRank {
-	HashMap<Integer, ArrayList<Integer>> incomingVertices;
-	HashMap<Integer, Double> values;
-	Digraph dg;
-	int outDegree;
-	int inDegree;
-	PageRank(Digraph digraph, Integer vertice) {
-		this.outDegree = digraph.outdegree(vertice);
-		this.inDegree = digraph.indegree(vertice);
-		this.dg = digraph;
-		incomingVertices = new HashMap<Integer, ArrayList<Integer>>();
-		for (int i = 0; i < digraph.V(); i++) {
-			for (Integer k : digraph.adj(i)) {
-				if (incomingVertices.containsKey(k)) {
-					ArrayList<Integer> arrayList2 = incomingVertices.get(k);
-					arrayList2.add(i);
-					//System.out.println("list :" + list);
-					incomingVertices.put(k, arrayList2);
-				} else {
-					ArrayList<Integer> arrayList3 = new ArrayList<Integer>();
-					arrayList3.add(i);
-					incomingVertices.put(k, arrayList3);
+	HashMap<Integer, Double>map;
+	Digraph graph;
+	Digraph revGraph;
+	int vertices;
+	PageRank(Digraph dg) {
+		graph=dg;
+		vertices = dg.V();
+		map = new HashMap<Integer, Double>();
+		revGraph = graph.reverse();
+	}
+	public void calculatePr() {
+		Double sum = 0.0;
+		int count = 0;
+		double temp = (double) vertices;
+		double initialPr = (1/temp);
+		for(int i = 0; i < vertices; i++) {
+			if(graph.indegree(i) == 0) {
+				map.put(i, 0.0);
+			} else {
+				map.put(i, initialPr);
+			}
+		}
+		double[] tempArray = new double[graph.V()];
+		for( int j = 0;j < 1000; j++) {
+			for( int i = 0; i < vertices; i++) {
+				sum = 0.0000;
+				for(int each : revGraph.adj(i)) {
+					double value = map.get(i);
+					sum += ((double)value/(double)graph.outdegree(each));
 				}
+				tempArray[i] = sum;
+			}
+			for(int i = 0; i < vertices; i++) {
+				map.put(i, tempArray[i]);
 			}
 		}
 	}
-	double getPR(int vertice) {
-		if (dg.outdegree(vertice) == 0) {
-			return 0.0;
+	public void print() {
+		for( int i = 0; i<map.size(); i++) {
+			System.out.println(i + "-" + map.get(i));
 		}
-		values = new HashMap<Integer, Double>();
-		for (int i = 0; i < dg.V(); i++) {
-			values.put(i , 1.0 / dg.V());
-		}
-		double fvalue = 0.0;
-		for (int i = 0; i < 1000; i++) {
-			ArrayList<Integer> vert = incomingVertices.get(vertice);
-			for (int j = 0; j < vert.size(); j++) {
-				int key = vert.get(j);
-				fvalue = values.get(key) / dg.outdegree(key);
-				values.put(key , fvalue);
-			}
-
-		}
-		return fvalue;
-
 	}
-
 }
 
 class WebSearch {
@@ -65,38 +60,46 @@ public class Solution {
 		//sc.nextLine();
 		Digraph d = new Digraph(totalVertices);
 		int i = 0;
-		while(i < totalVertices) {
+		while (i < totalVertices) {
 			String[] arra1 = sc.nextLine().split(" ");
-			for(int j = 1;j < arra1.length; j++) {
+			for (int j = 1; j < arra1.length; j++) {
 				d.addEdge(Integer.parseInt(arra1[0]), Integer.parseInt(arra1[j]));
 			}
 			i++;
-		}	
-		System.out.println(d.toString());	
-		// Create page rank object and pass the graph object to the constructor
-		
-		// print the page rank object
-		
-		// This part is only for the final test case
-		
-		// File path to the web content
-		ArrayList<PageRank> arrayList1 = new ArrayList<>();
-		for (int a = 0; a < totalVertices; a++) {
-			PageRank prObject = new PageRank(d, a);
-			arrayList1.add(prObject);
-			//System.out.println(a);
-			prObject.getPR(a);
-			
 		}
+		System.out.println(d.toString());
+		for (int k = 0; k < d.V(); k++) {
+			//System.out.println(d.adj(k));
+			if (d.outdegree(k) == 0) {
+				//System.out.println("i am here");
+				for (int a = 0; a < d.V(); a++ ) {
+					if (k != a) {
+						d.addEdge(k, a);
+					}
+				}
+			}
+		}
+		PageRank pr = new PageRank(d);
+		pr.calculatePr();
+		pr.print();
+		//System.out.println(d.toString());
+		//System.out.println(d.reverse());
+		// Create page rank object and pass the graph object to the constructor
+
+		// print the page rank object
+
+		// This part is only for the final test case
+
+		// File path to the web content
 		String file = "WebContent.txt";
-		
+
 		// instantiate web search object
 		// and pass the page rank object and the file path to the constructor
-		
+
 		// read the search queries from std in
 		// remove the q= prefix and extract the search word
 		// pass the word to iAmFeelingLucky method of web search
 		// print the return value of iAmFeelingLucky
-		
+
 	}
 }
