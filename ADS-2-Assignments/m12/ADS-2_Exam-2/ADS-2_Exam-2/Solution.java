77122127373d985114e3c6cac152e477b9df4a2b
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-11-10 11:46:24 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m12/ADS-2_Exam-2/ADS-2_Exam-2/Solution.java
+++ ADS-2-Assignments/m12/ADS-2_Exam-2/ADS-2_Exam-2/Solution.java
@@ -1,63 +1,68 @@
 import java.util.Scanner;
-import java.util.Arrays;
 /**
  * Class for solution.
  */
 public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
 /**
  * main function reads input.
  *
  * @param      args  The arguments
  */
-	public static void main(String[] args) {
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
-		Scanner sc = new Scanner(System.in);
-		int nCities = Integer.parseInt(sc.nextLine());
-		int nRoutes = Integer.parseInt(sc.nextLine());
-		EdgeWeightedGraph graph = new EdgeWeightedGraph(nCities);
-		int i = 0;
-		while (i < nRoutes){
-			String[] arra1 = sc.nextLine().split(" ");
-			graph.addEdge(new Edge(Integer.parseInt(arra1[0]),
-			 Integer.parseInt(arra1[1]), Integer.parseInt(arra1[2])));
-			i++;		
-		}
-		String caseToGo = sc.nextLine();
-		switch (caseToGo) {
-		case "Graph":
-		System.out.println(graph.toString());
-			
-			break;
+    public static void main(final String[] args) {
+        // Self loops are not allowed...
+        // Parallel Edges are allowed...
+        // Take the Graph input here...
+        Scanner sc = new Scanner(System.in);
+        int nCities = Integer.parseInt(sc.nextLine());
+        int nRoutes = Integer.parseInt(sc.nextLine());
+        EdgeWeightedGraph graph = new EdgeWeightedGraph(nCities);
+        int i = 0;
+        while (i < nRoutes) {
+            String[] arra1 = sc.nextLine().split(" ");
+            graph.addEdge(new Edge(Integer.parseInt(arra1[0]),
+             Integer.parseInt(arra1[1]), Integer.parseInt(arra1[2])));
+            i++;        
+        }
+        String caseToGo = sc.nextLine();
+        switch (caseToGo) {
+        case "Graph":
+        System.out.println(graph.toString());
+            
+            break;
 
-		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are
-			// given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			String[] arra2 = sc.nextLine().split(" ");
-			DijkstraUndirectedSP dj = new DijkstraUndirectedSP(graph, Integer.parseInt(arra2[0]));
-			if (dj.hasPathTo(Integer.parseInt(arra2[1]))) {
-				System.out.println(dj.distTo(Integer.parseInt(arra2[1])));
-			} else {
-				System.out.println("No Path Found.");
-			}
-			break;
+        case "DirectedPaths":
+            // Handle the case of DirectedPaths, where two integers are
+            // given.
+            // First is the source and second is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            String[] arra2 = sc.nextLine().split(" ");
+            DijkstraUndirectedSP dj = new DijkstraUndirectedSP(graph, Integer.parseInt(arra2[0]));
+            if (dj.hasPathTo(Integer.parseInt(arra2[1]))) {
+                System.out.println(dj.distTo(Integer.parseInt(arra2[1])));
+            } else {
+                System.out.println("No Path Found.");
+            }
+            break;
 
-		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where
-			// path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			break;
+        case "ViaPaths":
+            // Handle the case of ViaPaths, where three integers are given.
+            // First is the source and second is the via is the one where
+            // path should pass throuh.
+            // third is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            break;
 
-		default:
-			break;
-		}
+        default:
+            break;
+        }
 
-	}
+    }
 }
