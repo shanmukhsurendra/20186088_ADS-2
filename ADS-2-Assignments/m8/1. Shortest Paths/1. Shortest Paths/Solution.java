5166ddcec4af1f2a168337023908b5938e5cd0f0
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-11-14 12:27:08 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m8/1. Shortest Paths/1. Shortest Paths/Solution.java
+++ ADS-2-Assignments/m8/1. Shortest Paths/1. Shortest Paths/Solution.java
@@ -1,34 +1,45 @@
 import java.util.Scanner;
 import java.util.HashMap;
-
+/**
+ * Class for solution.
+ */
 class Solution {
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		sc.nextLine();
-		HashMap<String, Integer> map1 = new HashMap<>();
-		String arar1[] = sc.nextLine().split(" ");
-		for(int i = 0; i < n; i++) {
-			map1.put(arar1[i], i);
-		}
-		EdgeWeightedGraph graph1 = new EdgeWeightedGraph(n);
-		for ( int j = 0; j < m; j++) {
-		String[] arra2 = sc.nextLine().split(" ");
-		graph1.addEdge(new Edge(map1.get(arra2[0]), map1.get(arra2[1]), Double.parseDouble(arra2[2])));			
-		}
-		int a = sc.nextInt();
-		sc.nextLine();
-		for (int k = 0; k < a; k++) {
-			String arra3[] = sc.nextLine().split(" ");
-			DijkstraUndirectedSP dj = new DijkstraUndirectedSP(graph1,
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine();
+        HashMap<String, Integer> map1 = new HashMap<>();
+        String arar1[] = sc.nextLine().split(" ");
+        for(int i = 0; i < n; i++) {
+            map1.put(arar1[i], i);
+        }
+        EdgeWeightedGraph graph1 = new EdgeWeightedGraph(n);
+        for ( int j = 0; j < m; j++) {
+        String[] arra2 = sc.nextLine().split(" ");
+        graph1.addEdge(new Edge(map1.get(arra2[0]), map1.get(arra2[1]),
+         Double.parseDouble(arra2[2])));            
+        }
+        int a = sc.nextInt();
+        sc.nextLine();
+        for (int k = 0; k < a; k++) {
+            String arra3[] = sc.nextLine().split(" ");
+            DijkstraUndirectedSP dj = new DijkstraUndirectedSP(graph1,
                     map1.get(arra3[0]));
-			            System.out.println(Math.round(dj.distTo(map1.get(arra3[1]))));
+            System.out.println(Math.round(dj.distTo(map1.get(arra3[1]))));
 
-		}
-	}
+        }
+    }
 
 }
