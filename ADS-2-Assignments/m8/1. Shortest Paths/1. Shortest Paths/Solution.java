6d60807316d002e3f6788a908ba9098ebcdf7e43
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-11-14 12:29:36 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-Assignments/m8/1. Shortest Paths/1. Shortest Paths/Solution.java
+++ ADS-2-Assignments/m8/1. Shortest Paths/1. Shortest Paths/Solution.java
@@ -7,7 +7,7 @@ class Solution {
     /**
      * Constructs the object.
      */
-    Solution() {
+    private Solution() {
 
     }
     /**
@@ -15,26 +15,26 @@ class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         sc.nextLine();
         HashMap<String, Integer> map1 = new HashMap<>();
-        String arar1[] = sc.nextLine().split(" ");
-        for(int i = 0; i < n; i++) {
+        String[] arar1 = sc.nextLine().split(" ");
+        for (int i = 0; i < n; i++) {
             map1.put(arar1[i], i);
         }
         EdgeWeightedGraph graph1 = new EdgeWeightedGraph(n);
-        for ( int j = 0; j < m; j++) {
+        for (int j = 0; j < m; j++) {
         String[] arra2 = sc.nextLine().split(" ");
         graph1.addEdge(new Edge(map1.get(arra2[0]), map1.get(arra2[1]),
-         Double.parseDouble(arra2[2])));            
+         Double.parseDouble(arra2[2])));
         }
         int a = sc.nextInt();
         sc.nextLine();
         for (int k = 0; k < a; k++) {
-            String arra3[] = sc.nextLine().split(" ");
+            String[] arra3 = sc.nextLine().split(" ");
             DijkstraUndirectedSP dj = new DijkstraUndirectedSP(graph1,
                     map1.get(arra3[0]));
             System.out.println(Math.round(dj.distTo(map1.get(arra3[1]))));
@@ -43,3 +43,5 @@ class Solution {
     }
 
 }
+
+
